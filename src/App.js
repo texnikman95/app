dc56fbb250ea7c810216df53dbce6435e639f990
asaloliynemat@gmail.com <asaloliynemat@gmail.com>
2024-08-28 22:24:28 +0500

    web appga react ornatyapmiz

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          salom Azimjon9515
+          salom shaxrizoda
         </a>
       </header>
     </div>
